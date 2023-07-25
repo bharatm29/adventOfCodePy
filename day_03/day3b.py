69c31e68eeb9260d649855d00b21c73fb9889230
xZCute OP <bharatmhes@gmail.com>
2023-07-25 23:31:30 +0530

    Completed day_03 part two onto day_04

---
+++ day_03/day3b.py
@@ -0,0 +1,43 @@
+data = open("./input.txt").read().split('\n')
+dictOfItems = {}
+
+counter = 1
+str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for c in str:
+    dictOfItems[c] = counter
+    counter += 1
+
+ans = 0
+i = 0
+
+while i in range(len(data) - 2):
+    line1 = data[i]
+    line2 = data[i + 1]
+    line3 = data[i + 2]
+    i += 3
+
+    print("lines")
+    print(line1, line2, line3)
+    print("lines")
+
+    ch = '\0'
+
+    st1 = set()
+    st2 = set()
+
+    for c in line1:
+        st1.add(c)
+    
+    for c in line2:
+        if c in st1:
+            st2.add(c)
+    
+    for c in line3:
+        if c in st2:
+            ch = c
+            break
+
+    ans += dictOfItems[c]
+
+print(ans)
