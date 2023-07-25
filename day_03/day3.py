ca7b299f3c513e5622162ed5dd31ac65520b99e3
xZCute OP <bharatmhes@gmail.com>
2023-07-25 23:07:23 +0530

    Understood day03 first part onto second part

---
+++ day_03/day3.py
@@ -0,0 +1,32 @@
+data = open("./input.txt").read().split('\n')
+dictOfItems = {}
+
+counter = 1
+str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for c in str:
+    dictOfItems[c] = counter
+    counter += 1
+
+ans = 0
+
+# for line in data:
+#     size = len(line)
+#     half = int(size/2)
+#     print(half)
+
+for line in data:
+    size = len(line)
+    half = int(size / 2)
+
+    st = set()
+
+    for i in range(half):
+        for j in range(half, size):
+            if line[i] == line[j]:
+                st.add(line[i])
+    
+    for item in st:
+        ans += dictOfItems[item]
+
+print(ans)
